2db30fb30f558d336e0a7bfd637f47aa47038fe8
Thegrasscutter <94039953+Thegrasscutter@users.noreply.github.com>
2023-09-07 11:12:13 +0200

    Update oneclick.sh

--- oneclick.sh
+++ oneclick.sh
@@ -48,4 +48,5 @@ done
 
 #install ngrok
 echo "installing ngrok"
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt install ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list 
+apt update && apt install ngrok
